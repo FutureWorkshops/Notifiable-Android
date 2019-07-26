7284080e4ee3e5116ff18305d16a03311acec547
Stelian Morariu <stelian.morariu@gmail.com>
2019-07-26 10:49:57 +0100

    add kts dependency management

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,15 @@
+/*
+ * Copyright ©  2018 - 2019 FutureWorkshops. All rights reserved.
+ */
+
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    jcenter()
+}
+
+kotlinDslPluginOptions {
+    experimentalWarning.set(false)
+}
