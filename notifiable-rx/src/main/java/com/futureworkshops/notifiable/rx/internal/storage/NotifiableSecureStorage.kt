b832b19eccb3d27ba1ecceb2d804d4ad34acd2c3
stelian <stelian.morariu@gmail.com>
2019-07-31 12:21:30 +0100

    implement secure storage

--- notifiable-rx/src/main/java/com/futureworkshops/notifiable/rx/internal/storage/NotifiableSecureStorage.kt
+++ notifiable-rx/src/main/java/com/futureworkshops/notifiable/rx/internal/storage/NotifiableSecureStorage.kt
@@ -8,12 +8,16 @@ package com.futureworkshops.notifiable.rx.internal.storage
 import android.content.SharedPreferences
 import androidx.security.crypto.EncryptedSharedPreferences
 import com.futureworkshops.notifiable.rx.model.NotifiableDevice
+import com.google.gson.Gson
+import timber.log.Timber
 
 
 /**
  * This local storage class uses [EncryptedSharedPreferences] to store data.
  */
-class NotifiableSecureStorage(sharedPreferences: SharedPreferences) {
+class NotifiableSecureStorage(private val sharedPreferences: SharedPreferences) {
+    private val gson = Gson()
+
     init {
         if (sharedPreferences !is EncryptedSharedPreferences) {
             throw RuntimeException("Notifiable secure storage requires androidx.security.crypto.EncryptedSharedPreferences")
@@ -21,10 +25,31 @@ class NotifiableSecureStorage(sharedPreferences: SharedPreferences) {
     }
 
     fun saveNotifiableDevice(notifiableDevice: NotifiableDevice) {
-
+        sharedPreferences.edit()
+            .putString(NOTIFIABLE_DEVICE_KEY, gson.toJson(notifiableDevice))
+            .apply()
     }
 
     fun removeNotifiableDevice() {
+        sharedPreferences.edit().clear().apply()
+    }
 
+    fun getRegisteredDevice(): NotifiableDevice? {
+        val json = sharedPreferences.getString(NOTIFIABLE_DEVICE_KEY, "")
+
+        json?.let {
+            return try {
+                gson.fromJson(it, NotifiableDevice::class.java)
+            } catch (e: Exception) {
+                Timber.e(e)
+                null
+            }
+        }
+
+        return null
+    }
+
+    companion object {
+        private const val NOTIFIABLE_DEVICE_KEY = "notifiable_device"
     }
 }
