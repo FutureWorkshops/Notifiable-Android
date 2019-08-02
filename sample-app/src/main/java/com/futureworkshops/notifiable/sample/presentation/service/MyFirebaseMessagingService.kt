18e88ee4e0fe6a43d4dc5ee395da43a134bba31c
stelian <stelian.morariu@gmail.com>
2019-08-02 16:04:08 +0100

    inject service

--- sample-app/src/main/java/com/futureworkshops/notifiable/sample/presentation/service/MyFirebaseMessagingService.kt
+++ sample-app/src/main/java/com/futureworkshops/notifiable/sample/presentation/service/MyFirebaseMessagingService.kt
@@ -11,23 +11,22 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.media.RingtoneManager
 import android.os.Build
 import android.os.Build.VERSION_CODES
-import android.preference.PreferenceManager
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.futureworkshops.notifiable.rx.NotifiableManagerRx
 import com.futureworkshops.notifiable.rx.internal.createNotificationFromMap
 import com.futureworkshops.notifiable.rx.model.NotifiableMessage
-import com.futureworkshops.notifiable.sample.BuildConfig
 import com.futureworkshops.notifiable.sample.Constants
 import com.futureworkshops.notifiable.sample.R
-import com.futureworkshops.notifiable.sample.presentation.demo.DemoActivity
+import com.futureworkshops.notifiable.sample.presentation.notification.NotificationActivity
+import com.futureworkshops.notifiable.sample.presentation.notification.NotificationActivity.Companion.NOTIFICATION
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import dagger.android.AndroidInjection
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
@@ -36,25 +35,13 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     private lateinit var notifiableManagerRx: NotifiableManagerRx
 
-    private var sharedPrefs: SharedPreferences? = null
-
     override fun onCreate() {
         super.onCreate()
+        AndroidInjection.inject(this)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createNotificationChannel()
         }
-
-        sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this)
-
-
-        notifiableManagerRx = NotifiableManagerRx.Builder(this)
-            .endpoint(BuildConfig.NOTIFIABLE_SERVER)
-            .credentials(
-                BuildConfig.NOTIFIABLE_CLIENT_ID,
-                BuildConfig.NOTIFIABLE_CLIENT_SECRET
-            )
-            .debug(BuildConfig.DEBUG)
-            .build()
 
     }
 
@@ -80,9 +67,9 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
         markNotificationAsReceived(notification)
 
-        val intent = Intent(this, DemoActivity::class.java)
+        val intent = Intent(this, NotificationActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-        intent.putExtra(Constants.NOTIFICATION, notification)
+        intent.putExtra(NOTIFICATION, notification)
 
         val pendingIntent = PendingIntent.getActivity(
             this, 0 /* Request code */, intent,
@@ -106,9 +93,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     @SuppressLint("CheckResult")
     private fun markNotificationAsReceived(notification: NotifiableMessage) {
-        val deviceId = sharedPrefs!!.getInt(Constants.NOTIFIABLE_DEVICE_ID, -1)
-
-
         notifiableManagerRx.markNotificationReceived(notification.notificationId.toString())
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
