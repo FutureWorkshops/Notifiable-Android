341a2926e230e41f5f23ff3e4d5a519c0e61361f
stelian <stelian.morariu@gmail.com>
2019-08-05 17:30:47 +0100

    create view state and view model

--- sample-app/src/main/java/com/futureworkshops/notifiable/sample/presentation/notification/NotificationViewModel.kt
+++ sample-app/src/main/java/com/futureworkshops/notifiable/sample/presentation/notification/NotificationViewModel.kt
@@ -4,21 +4,27 @@
 
 package com.futureworkshops.notifiable.sample.presentation.notification
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.futureworkshops.notifiable.rx.NotifiableManagerRx
+import com.futureworkshops.notifiable.rx.model.NotifiableMessage
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 import javax.inject.Inject
 
 class NotificationViewModel @Inject constructor(private val notifiableManagerRx: NotifiableManagerRx) :
     ViewModel() {
 
-
     val viewState: LiveData<NotificationViewState>
         get() = _viewState
 
     private val _viewState: MutableLiveData<NotificationViewState> = MutableLiveData()
+
+    private val _notificationLiveData: MutableLiveData<NotifiableMessage> = MutableLiveData()
 
     private val disposables = CompositeDisposable()
 
@@ -26,5 +32,41 @@ class NotificationViewModel @Inject constructor(private val notifiableManagerRx:
     override fun onCleared() {
         disposables.clear()
         super.onCleared()
+    }
+
+    @SuppressLint("CheckResult")
+    fun setNotification(notifiableMessage: NotifiableMessage) {
+        // save notification locally
+        _notificationLiveData.postValue(notifiableMessage)
+
+        // display notification
+        _viewState.postValue(
+            NotificationViewState(
+                displayNotification = true,
+                notification = notifiableMessage
+            )
+        )
+
+        // mark notification as opened
+        notifiableManagerRx.markNotificationOpened(notifiableMessage.notificationId.toString())
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { _viewState.postValue(NotificationViewState(isMarkingNotification = true)) }
+            .subscribe(
+                {
+                    _viewState.postValue(NotificationViewState(isNotificationMarked = true))
+
+                },
+                { t ->
+                    Timber.e(t)
+                    _viewState.postValue(
+                        NotificationViewState(
+                            hasError = true,
+                            errorMessage = t.message
+                        )
+                    )
+                }
+            )
+
     }
 }
--- sample-app/src/main/java/com/futureworkshops/notifiable/sample/presentation/notification/NotificationViewState.kt
+++ sample-app/src/main/java/com/futureworkshops/notifiable/sample/presentation/notification/NotificationViewState.kt
@@ -4,5 +4,13 @@
 
 package com.futureworkshops.notifiable.sample.presentation.notification
 
-class NotificationViewState {
-}
+import com.futureworkshops.notifiable.rx.model.NotifiableMessage
+
+data class NotificationViewState(
+    val displayNotification: Boolean = false,
+    val isMarkingNotification: Boolean = false,
+    val isNotificationMarked: Boolean = false,
+    val notification: NotifiableMessage? = null,
+    val hasError: Boolean = false,
+    val errorMessage: String? = null
+)
